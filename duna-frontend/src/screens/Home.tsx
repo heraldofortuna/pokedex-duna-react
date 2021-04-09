5ec17ae36ad4981b94479f7e824de5dbf0a85474
heraldofortuna <heraldofortuna@gmail.com>
2021-04-09 12:27:29 -0500

    I created a general styled wrapper for all views

--- duna-frontend/src/screens/Home.tsx
+++ duna-frontend/src/screens/Home.tsx
@@ -1,5 +1,19 @@
+import styled from "styled-components";
+
+const StyledWrapper = styled.div`
+  background-color: var(--white);
+  width: 50%;
+  min-height: 100vh;
+  padding: 20px;
+  margin: 0 auto;
+`;
+
 function Home() {
-  return <h1>Pokedex</h1>;
+  return (
+    <StyledWrapper>
+      <h1>Pokedex</h1>
+    </StyledWrapper>
+  );
 }
 
 export default Home;
