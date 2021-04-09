3508e23c077364f073373193168ead80aa962c40
heraldofortuna <heraldofortuna@gmail.com>
2021-04-09 12:19:33 -0500

    I implemented all global styles

--- duna-frontend/src/globalStyles.tsx
+++ duna-frontend/src/globalStyles.tsx
@@ -1,11 +1,27 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: green;
-    font-family: Open-Sans, Helvetica, Sans-Serif;
+  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap');
+
+  :root {
+    --white: #FFFFFF;
+    --black: #303943;
+    --gray: #B3B6BA;
+    --light-gray: #D9DFE9;
+    --blue: #98A2E6;
+    --pink: #F5A5C2;
+  }
+
+  * {
+    color: var(--black);
+    font-family: 'Roboto', sans-serif;
+    box-sizing: border-box;
     padding: 0;
     margin: 0;
+  }
+
+  body {
+    background-color: var(--light-gray);
   }
 `;
 
