3626480c12c71a99a4f0156809651310aee59c49
heraldofortuna <heraldofortuna@gmail.com>
2021-04-09 11:51:12 -0500

    I initialized globalStyles file and I added types/styled-components to this project

---
+++ duna-frontend/src/globalStyles.tsx
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: green;
+    font-family: Open-Sans, Helvetica, Sans-Serif;
+    padding: 0;
+    margin: 0;
+  }
+`;
