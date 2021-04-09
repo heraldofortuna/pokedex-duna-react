a4c1db1b7349011cc8fbcfc330e0c5fb3b2fb176
heraldofortuna <heraldofortuna@gmail.com>
2021-04-09 12:51:23 -0500

    I implemented axios correctly to consume the Pokemon API and I fixed the import google fonts

--- duna-frontend/src/globalStyles.tsx
+++ duna-frontend/src/globalStyles.tsx
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap');
-
   :root {
     --white: #FFFFFF;
     --black: #303943;
--- duna-frontend/src/screens/Home.tsx
+++ duna-frontend/src/screens/Home.tsx
@@ -1,4 +1,15 @@
+import React from "react";
 import styled from "styled-components";
+import axios from "axios";
+
+interface IPost {
+  id: number;
+  userId?: number;
+  title: string;
+  body: string;
+}
+
+const defaultPosts: IPost[] = [];
 
 const StyledWrapper = styled.div`
   background-color: var(--white);
@@ -9,9 +20,56 @@ const StyledWrapper = styled.div`
 `;
 
 function Home() {
+  const [posts, setPosts]: [IPost[], (posts: IPost[]) => void] = React.useState(
+    defaultPosts
+  );
+
+  const [loading, setLoading]: [
+    boolean,
+    (loading: boolean) => void
+  ] = React.useState<boolean>(true);
+
+  const [error, setError]: [string, (error: string) => void] = React.useState(
+    ""
+  );
+
+  React.useEffect(() => {
+    axios
+      .get<IPost[]>("https://pokeapi.co/api/v2/pokemon/", {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        timeout: 4000,
+      })
+      .then((response) => {
+        setPosts(response.data);
+        setLoading(false);
+      })
+      .catch((ex) => {
+        const error =
+          ex.code === "ECONNABORTED"
+            ? "A timeout has occurred"
+            : ex.response.status === 404
+            ? "Resource not found"
+            : "An unexpected error has occurred";
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <StyledWrapper>
       <h1>Pokedex</h1>
+      {/* <ul className="posts">
+        {posts.map((post) => (
+          <li key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.body}</p>
+          </li>
+        ))}
+      </ul>
+      {error && <p className="error">{error}</p>} */}
+      {console.log(posts)}
     </StyledWrapper>
   );
 }
