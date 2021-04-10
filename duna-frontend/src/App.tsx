3b9e89f744c24def7f4467a7bcfb2df549521a19
heraldofortuna <heraldofortuna@gmail.com>
2021-04-09 23:44:26 -0500

    I added a react-router-dom dependency

--- duna-frontend/src/App.tsx
+++ duna-frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import GlobalStyle from "./globalStyles";
 import Home from "./screens/Home";
 
