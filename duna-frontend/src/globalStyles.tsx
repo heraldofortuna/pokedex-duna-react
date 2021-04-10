a88223956e42ca21588feb7d8bd4af3415736edb
heraldofortuna <heraldofortuna@gmail.com>
2021-04-10 17:18:21 -0500

    I added all colors iin globalStyles and poke types in backgroundColorPokemon function

--- duna-frontend/src/globalStyles.tsx
+++ duna-frontend/src/globalStyles.tsx
@@ -7,10 +7,17 @@ const GlobalStyle = createGlobalStyle`
     --gray: #B3B6BA;
     --light-gray: #D9DFE9;
     --blue: #98A2E6;
+    --light-blue: #6ED3D3;
     --green: #48D0B0;
-    --red: #FB6C6C;
+    --light-green: #48D065;
+    --red: #D7782E;
+    --light-red: #FB6C6C;
     --pink: #F5A5C2;
-    --yellow: #FFD86F;
+    --yellow: #DBD759;
+    --light-yellow: #FFD86F;
+    --purple: #7F668C;
+    --light-purple: #9E74C3;
+    --brown: #9B7B61;
   }
 
   * {
--- duna-frontend/src/screens/Pokemon.tsx
+++ duna-frontend/src/screens/Pokemon.tsx
@@ -72,14 +72,58 @@ export default function Pokemon() {
   }, [pokeName]);
 
   function backgroundColorPokemon(type: any) {
-    let backgroundColor = "var(--blue)";
+    let backgroundColor = "var(--white)";
     switch (type) {
       case "grass": {
         backgroundColor = "var(--green)";
         break;
       }
       case "fire": {
+        backgroundColor = "var(--light-red)";
+        break;
+      }
+      case "water": {
+        backgroundColor = "var(--blue)";
+        break;
+      }
+      case "bug": {
+        backgroundColor = "var(--light-green)";
+        break;
+      }
+      case "normal": {
+        backgroundColor = "var(--gray)";
+        break;
+      }
+      case "poison": {
+        backgroundColor = "var(--light-purple)";
+        break;
+      }
+      case "electric": {
+        backgroundColor = "var(--light-yellow)";
+        break;
+      }
+      case "ground": {
+        backgroundColor = "var(--yellow)";
+        break;
+      }
+      case "fighting": {
         backgroundColor = "var(--red)";
+        break;
+      }
+      case "psychic": {
+        backgroundColor = "var(--pink)";
+        break;
+      }
+      case "rock": {
+        backgroundColor = "var(--brown)";
+        break;
+      }
+      case "ghost": {
+        backgroundColor = "var(--purple)";
+        break;
+      }
+      case "ice": {
+        backgroundColor = "var(--light-blue)";
         break;
       }
       default: {
