a7d52d4ee9040f0e1086b32ae5603a985945705e
heraldofortuna <heraldofortuna@gmail.com>
2021-04-10 22:07:36 -0500

    I added a small image of pokeball in any pokeCard

--- duna-frontend/src/components/PokeCard.tsx
+++ duna-frontend/src/components/PokeCard.tsx
@@ -1,8 +1,14 @@
-import styled from "styled-components";
 import { IPokeCard } from "../services/Interfaces";
+
+import styled from "styled-components";
+import PokeballImage from "../assets/pokeball.svg";
 
 const StyledPokeCard = styled.li`
   background-color: var(--white);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
   border: 2px solid var(--black);
   box-shadow: -2px 2px 0px var(--gray);
   padding: 8px;
@@ -16,12 +22,18 @@ const StyledPokeCard = styled.li`
   & > p {
     text-transform: capitalize;
   }
+
+  .pokecard__image {
+    width: 20px;
+    height: 20px;
+  }
 `;
 
 export default function PokeCard(prop: IPokeCard) {
   return (
     <StyledPokeCard>
       <p>{prop.children}</p>
+      <img className="pokecard__image" src={PokeballImage} alt="Pokeball" />
     </StyledPokeCard>
   );
 }
